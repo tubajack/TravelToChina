eba44847543485ff2a30675bdfb1312815f7aa8e
Jackson Henry <jackson.henry13@yahoo.com>
2019-08-10 13:12:14 -0500

    Uploaded a picture of Chinese ethnic groups.

--- client/src/pages/ChineseCulture.js
+++ client/src/pages/ChineseCulture.js
@@ -8,7 +8,19 @@ export default class ChineseCulture extends Component {
         <h1>Chinese Culture</h1>  
         <Navbar /> 
 
-             
+        <h2>Ethnic Groups</h2>
+        <img
+          id="Ethnic Groups in China"
+          src="../images/EthnicGroupsofChina.jpg"
+          alt="Ethnic Groups"
+          width="100%"
+          height="100%"
+        />   
+        <p>
+           China has 56 different ethnic groups. 
+           The most common ethnic group is the Han Chinese which accounts for 91.64% of all Chinese. 
+           Other ethnic groups include Zhuang, Hui, Man, Ugyur, etc.
+        </p>  
       </div>
     );
   }
