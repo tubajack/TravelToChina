f5eab9faafb9cd3c3f00ea147f4b2e11bb8d6964
Jackson Henry <jackson.henry13@yahoo.com>
2019-08-10 10:29:58 -0500

    Set up titles for each of the individual webpages. The next step is going to be putting navbars in each of these pages.

---
+++ client/src/pages/ChineseCulture.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ChineseCulture extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Chinese Culture</h1>           
+      </div>
+    );
+  }
+}
---
+++ client/src/pages/Geography.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Geography extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Geography of China</h1>           
+      </div>
+    );
+  }
+}
