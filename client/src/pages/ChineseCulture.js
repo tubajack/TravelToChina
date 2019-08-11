9604dfda3b1b9cc9f938f654b1bde82dd7d4225d
Jackson Henry <jackson.henry13@yahoo.com>
2019-08-11 18:54:42 -0500

    Added some more information about the language in China.

--- client/src/pages/ChineseCulture.js
+++ client/src/pages/ChineseCulture.js
@@ -35,6 +35,22 @@ export default class ChineseCulture extends Component {
           <li>Main religions in China incude Buddhism, Christianity, Taoism, Islam, and Confucianism</li>
           <li>Many people in China practice Buddhism, Confucianism, and Taoism</li>
         </ul>
+
+        <h2>Language in China</h2>
+        <img
+          id="Religion in China"
+          src="../images/ChineseLanguage.jpg"
+          alt="Ethnic Groups"
+          width="90%"
+          height="90%"
+        /> 
+        <ul>
+          <li>About 1.2 Billion people speak some form of Chinese as their first language.</li>
+          <li>There are between 7 and 13 regional groups of Chinese. Mandarin is the most common dialect followed by Wu, Yue, Cantonese etc.</li>
+          <li>All dialects of Chinese are tonal and analytic.</li>
+          <li>Standard Chinese, based on the Beijing dialect of Mandarin (Putonghua), is the official language of China and Taiwan.</li>
+          <li>In addition, it is one of the six official lanuages of the UN.</li>
+        </ul>
       </div>
     );
   }
