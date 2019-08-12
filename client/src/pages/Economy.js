f43d37744df821394a49333fba4685f6fd1686e3
Jackson Henry <jackson.henry13@yahoo.com>
2019-08-11 20:18:57 -0500

    Added some more information about the Chinese economy. This information related to the basic history.

--- client/src/pages/Economy.js
+++ client/src/pages/Economy.js
@@ -8,7 +8,15 @@ export default class Economy extends Component {
         <h1>Economy of China</h1>  
         <Navbar />   
 
-        <p>The Chinese Economy is the world's second largest economy by both Nominal GDP and Purchasing Power Parity.</p>       
+        <h2>History of the Chinese Economy</h2>      
+
+        <p>
+          During the late 1970s, China moved to a more market-oriented economy. As a result, the Gross Domestic Product (GDP)
+          has increased significantly. From 2013 to 2017, China had one of the fastest growing economies in the world, averaging 
+          about 7% real growth annually. Although China experienced massive growth from 1978 to 2013, authoritieds have implemented
+          the "New Normal" which was implemented to avoid overheating. Although economic growth has slown down since 2010, the Chinese 
+          Economy is still projected to grow 6.9% in 2019.  
+        </p>
       </div>
     );
   }
