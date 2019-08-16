c38a6eb2423e7e76e5b3bcf747a7f1d8425a4e5c
Jackson Henry <jackson.henry13@yahoo.com>
2019-08-15 23:13:19 -0500

    Correcting the server.js file. Visual Studio is extremely anal about double and single quotes and doesnt like single quotes.

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const routes = require('./controllers');
+const express = require("express");
+const routes = require("./controllers");
 const app = express();
 const PORT = process.env.PORT || 3001;
-var db = require('./models');
+var db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
